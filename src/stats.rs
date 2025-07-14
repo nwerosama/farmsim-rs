49e0351082fe7fa273febd6aab46dbdacd94062b
nwerosama <96593068+nwerosama@users.noreply.github.com>
2025-07-14 20:00:24 +1000

    forgor serde rename

--- src/stats.rs
+++ src/stats.rs
@@ -79,6 +79,7 @@ pub struct Player {
 pub struct Vehicle {
   pub name:     Option<String>,
   pub category: Option<String>,
+  #[serde(rename = "type")]
   pub type_:    Option<String>,
   pub x:        Option<f32>,
   pub y:        Option<f32>,
