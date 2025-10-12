1c7e9ebab63222e598ddff39f35e6dce824ff767
nwerosama <96593068+nwerosama@users.noreply.github.com>
2025-10-12 21:35:00 +1100

    update utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,23 +1,43 @@
-pub fn format_daytime(day_time: i32) -> String {
-  let hours = day_time / 3_600_000;
-  let mins = (day_time % 3_600_000) / 60_000;
-  format!("{hours:02}:{mins:02}")
+use std::fmt::Write;
+
+pub fn fmt_daytime(daytime: i32) -> String {
+  let hours = daytime / 3_600_000;
+  let mins = (daytime % 3_600_000) / 60_000;
+
+  let mut s = String::with_capacity(5);
+  let _ = write!(&mut s, "{hours:02}:{mins:02}");
+  s
 }
 
-pub fn format_player_uptime(uptime: i32) -> String {
-  let mins: i32;
-  let mut hrs: i32 = 0;
+pub fn fmt_uptime(uptime: i32) -> String {
+  let mins = uptime % 60;
+  let hrs = uptime / 60;
 
-  if uptime >= 60 {
-    hrs = uptime / 60;
-    mins = uptime % 60;
+  let mut s = String::new();
+
+  if hrs > 0 {
+    let _ = write!(s, "{hrs} h");
+    if mins > 0 {
+      let _ = write!(s, " {mins} m");
+    }
   } else {
-    mins = uptime;
+    let _ = write!(s, "{mins} m");
   }
 
-  format!(
-    "{}{}",
-    if hrs > 0 { format!("{hrs} h ") } else { String::new() },
-    if mins > 0 { format!("{mins} m") } else { String::new() }
-  )
+  s
+}
+
+#[test]
+fn test_uptime() {
+  assert_eq!(fmt_uptime(135), "2 h 15 m");
+  assert_eq!(fmt_uptime(61), "1 h 1 m");
+  assert_eq!(fmt_uptime(59), "59 m");
+  assert_eq!(fmt_uptime(0), "0 m");
+}
+
+#[test]
+fn test_daytime() {
+  assert_eq!(fmt_daytime(9045683), "02:30");
+  assert_eq!(fmt_daytime(413975), "00:06");
+  assert_eq!(fmt_daytime(0), "00:00");
 }
